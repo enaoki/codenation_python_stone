74196b79459cf7ba763fc782db23a5be7dabb806
Ennio Miyazaki Aoki <enaoki@deloitte.com>
2020-05-29 20:41:04 -0300

    Solving python-7

--- python-7/main.py
+++ python-7/main.py
@@ -2,11 +2,17 @@ import requests
 
 
 def get_temperature(lat, lng):
+    if type(lat) != float or type(lng) != float:
+        raise TypeError("{0}/{1} invalid".format(lat, lng))
+
     key = 'e1ee55658d4a2b28c4841e373c3b3d87'
-    url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
+    #   define Système international as default
+    #   no need to convert from fahrenheit
+    unit = 'si'
+    url = 'https://api.darksky.net/forecast/{}/{},{}?unit={}'.format(key, lat, lng, unit)
     reponse = requests.get(url)
     data = reponse.json()
     temperature = data.get('currently').get('temperature')
     if not temperature:
         return
-    return int((temperature - 32) * 5.0 / 9.0)
+    return temperature
--- python-7/test_main.py
+++ python-7/test_main.py
@@ -1,5 +1,85 @@
 from main import get_temperature
+from unittest.mock import patch
+import pytest
+
+parametrized_values_equal = [
+    (16, -23.599228, -46.605443, 16),
+    (-10, -23.655846, -46.527415, -10)
+]
+
+parametrized_values_different = [
+    (5, 81.255032, -40.539859, 10),
+    (-5, 81.255032, -40.539859, -10),
+    (-5, 81.255032, -40.539859, 10)
+]
+
+parametrized_values_invalid = [
+    (26, 'lat', -124.236147, 20),
+    (36, 65.219894, 'long', 35),
+    (36, 65.219894, None, 35)
+]
+
+@pytest.mark.parametrize(
+    "temperature_from_api,lat,lng,temperature_result",
+    parametrized_values_equal
+)
+def test_should_be_equal(temperature_from_api, lat, lng, temperature_result):
+
+    temperature = {
+        "currently": {
+            "temperature": temperature_from_api
+        }
+    }
+
+    #   replace get function used in get_temperature
+    #   more info: https://realpython.com/python-mock-library/
+    with patch('main.requests.get') as mock_get:
+
+        #   define mock result
+        #   more info: https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock.return_value
+        mock_get.return_value.json.return_value = temperature
+
+        response = get_temperature(lat, lng)
+
+        assert response == temperature_result, "Should be equal"
+
+@pytest.mark.parametrize(
+    "temperature_from_api,lat,lng,temperature_result",
+    parametrized_values_different
+)
+def test_should_be_different(temperature_from_api, lat, lng, temperature_result):
+
+    temperature = {
+        "currently": {
+            "temperature": temperature_from_api
+        }
+    }
+
+    with patch('main.requests.get') as mock_get:
+
+        mock_get.return_value.json.return_value = temperature
+
+        response = get_temperature(lat, lng)
+
+        assert response != temperature_result, "Should be different"
 
 
-def test_get_temperature_by_lat_lng():
-    raise NotImplementedError
+@pytest.mark.parametrize(
+    "temperature_from_api,lat,lng,temperature_result",
+    parametrized_values_invalid
+)
+def test_invalid_input(temperature_from_api, lat, lng, temperature_result):
+
+    temperature = {
+        "currently": {
+            "temperature": temperature_from_api
+        }
+    }
+
+    with patch('main.requests.get') as mock_get:
+
+        mock_get.return_value.json.return_value = temperature
+
+        with pytest.raises(TypeError):
+
+            get_temperature(lat, lng)
