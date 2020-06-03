efa3c3515876f790957fc64471499ea4d580c292
Ennio Miyazaki Aoki <enaoki@deloitte.com>
2020-06-03 19:34:31 -0300

    Solved python-8

--- python-8/main.py
+++ python-8/main.py
@@ -1,9 +1,23 @@
-import jwt
+import jwt, logging
 
+logging.basicConfig(level=logging.DEBUG)
 
+# https://cheatsheetseries.owasp.org/cheatsheets/REST_Security_Cheat_Sheet.html
+# https://cheatsheetseries.owasp.org/cheatsheets/JSON_Web_Token_for_Java_Cheat_Sheet.html#token-storage-on-client-side
 def create_token(data, secret):
-    raise NotImplementedError()
+    return jwt.encode(data, secret, algorithm='HS256')
 
 
-def verify_signature(token):
-    raise NotImplementedError()
+def verify_signature(token, secret="acelera"):
+    try:
+        signature = jwt.decode(token, secret, algorithms='HS256')
+        return signature
+    except jwt.InvalidSignatureError as ex:
+        logging.error(ex)
+        return {"error": 2}
+    except jwt.InvalidTokenError as ex:
+        logging.error(ex)
+        return {"error": 2}
+    except Exception as ex:
+        logging.error(ex)
+        raise ex
--- python-8/test_main.py
+++ python-8/test_main.py
@@ -1,8 +1,42 @@
-from main import create_token
-
+from main import create_token, verify_signature
+import pytest
+from unittest import mock as mock
 
 class TestChallenge4:
     token = b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsYW5ndWFnZSI6IlB5dGhvbiJ9.sM_VQuKZe_VTlqfS3FlAm8XLFhgvQQLk2kkRTpiXq7M'
+    secret = "acelera"
 
     def test_create_token(self):
-        assert create_token({"language": "Python"}, "acelera") == self.token
+        assert create_token({"language": "Python"}, self.secret) == self.token
+
+    def test_valid_signature(self):
+        decoded = verify_signature(self.token, self.secret)
+        assert decoded.get('language') == 'Python'
+
+    def test_invalid_secret(self):
+        decoded = verify_signature(self.token, "acelera123")
+        assert decoded.get('error') == 2
+
+    @pytest.mark.parametrize(
+        "token",
+        ['testasd']
+    )
+    def test_verify_signature_fail_by_token(self, token):
+        decoded = verify_signature(token, self.secret)
+        assert decoded.get('error') == 2
+
+    @pytest.mark.parametrize(
+        "token",
+        [b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsYW5ndWFnZSI6IlJ1YnkifQ.IhyzOvcnUI0vJXPPIG6TkcG3kgN-sCfU5n-XG8jTzRU']
+    )
+    def test_verify_signature_fail_by_value(self, token):
+        decoded = verify_signature(token, self.secret)
+        assert decoded.get('language') != 'Python'
+
+    @mock.patch('main.jwt')
+    def test_exception(self, mock_jwt):
+        mock_jwt.decode.side_effect=Exception
+        
+        with pytest.raises(Exception):
+            verify_signature(self.token, self.secret)
+        
